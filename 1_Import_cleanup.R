2602a03c93c2bbb8268c8e628163f5c1ec7ab3d8
suzan baert <suzan.baert@outlook.com>
2018-07-27 23:43:12 +0200

    book 1 cleaned

--- 1_Import_cleanup.R
+++ 1_Import_cleanup.R
@@ -4,6 +4,7 @@
 
 library(stringr)
 library(dplyr)
+library(tidytext)
 
 
 
@@ -14,6 +15,7 @@ all_epubs <- paste0("epub_expanse/", all_epubs)
 
 #importing data just 1
 expanse1_epub <- epubr::epub(file = "epub_expanse/James SA Corey - The Expanse 01 - Leviathan Wakes.epub") 
+
 
 
 #importing all 
@@ -39,6 +41,7 @@ expanse1_text <- expanse1_text[4:61, ]
 split_header_from_text <- str_split(expanse1_text$text, "\n", n = 2)
 split_header_from_text <- purrr::transpose(split_header_from_text)
 
+chapter_text <- unlist(split_header_from_text[2])
 chapter_header <- unlist(split_header_from_text[1])
 
 #extracting point of view character from header
@@ -48,22 +51,38 @@ chapter_pov <- na_if(chapter_pov, "STATION")
 #extracting chapter number
 chapter <- str_remove(chapter_header, "Chapter ")
 chapter <- str_remove(chapter, ":.*")
-chapter_number2 <- c(NA, 1:35, NA, 36:55, NA)
+chapter_number <- c(NA, 1:35, NA, 36:55, NA)
 
 
-
-
-#chapter text
-chapter_text <- unlist(split_header_from_text[2])
-
-
+#fixing encoding issues
+this <- c("didn’t", "he’d", "don’t")
+chapter_text <- iconv(chapter_text, from="windows-1252", to="UTF-8")
+chapter_text <- str_replace(chapter_text, "â€™", "'")
 
 
 #back to dataframe
-expanse1 <- tibble(chapter = chapter_number2,
+expanse1 <- tibble(chapter_no = chapter_number,
+                   chapter = chapter,
                    pov = chapter_pov,
                    text = chapter_text)
 
 
+#unnest_tokens
+expanse1_tokens <- expanse1 %>% 
+  unnest_tokens(word, text) %>% 
+  add_count(chapter)
 
-expanse1$text
+
+
+
+
+
+#word count
+expanse1_tokens %>%
+  anti_join(stop_words, by = "word") %>% 
+  count(word, sort = TRUE) 
+
+
+
+
+
